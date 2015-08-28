9d1351dbb999e454545c0a1f5f4a80ec38f9cf35
David Cazalis <davidcazalis@gmail.com>
2015-08-28 16:43:11 +0200

    add browsersync, clean and jade templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// Add external config file
+var config = require('./config.json');
+
+var browserSync = require('browser-sync').create(),
+    del = require('del');
+
 // Gulp
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
@@ -8,24 +14,60 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
+    jade = require("gulp-jade"),
+    gulpif = require("gulp-if");
 
-    // Add external config file
-    config = require('./config.json');
-
-
+// ==========================================================================
 // Tasks
 // ==========================================================================
 
+function clean(path, files) {
+  gutil.log(gutil.colors.grey('Clean '+ files +' files.'));
+  del([path + '/*.' + files]);
+}
+
+// ==========================================================================
+// Tasks
+// ==========================================================================
+
+// Compile Sass files into CSS
 gulp.task('styles', function() {
-  return gulp.src(config.dev + config.styles.src)
-    .pipe(watch(config.dev + config.styles.src))
+  clean(config.styles.dest, 'css');
+  return gulp.src(config.styles.src)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
         browser: [config.browserslist],
-        cascade: false
-      }))
+        cascade: false }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest (config.dev + config.styles.dest))
+    .pipe(gulp.dest (config.styles.dest))
+    .pipe(browserSync.stream());
+});
+
+// Compile Jade files into HTML
+gulp.task('templates', function() {
+  clean(config.templates.dest, 'html');
+  return gulp.src(config.templates.src)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest (config.templates.dest))
+    .pipe(browserSync.stream());
+});
+
+// Create a server and watch files
+gulp.task('live', ['styles', 'templates'], function() {
+  browserSync.init({
+    startPath: config.templates.dest+'/index.html',
+    server: {
+      baseDir: './',
+      directory: true
+    }
+  });
+
+  gulp.watch(config.styles.src, ['styles']);
+  gulp.watch(config.templates.src, ['templates']);
+  gulp.watch(config.templates.dest+'/*.html').on('change', browserSync.reload);
 });
