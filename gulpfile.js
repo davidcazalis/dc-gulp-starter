eb05b8ea6c571edc3d3dc788362d139154fa8a12
David Cazalis <davidcazalis@gmail.com>
2015-09-01 16:47:53 +0200

    sass and templates build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@
 var config = require('./config.json');
 
 var browserSync = require('browser-sync').create(),
-    del = require('del');
+    del = require('del'),
+    options = require("minimist")(process.argv.slice(2));
 
 // Gulp
 var gulp = require('gulp'),
@@ -14,9 +15,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
-    jade = require("gulp-jade"),
-    gulpif = require("gulp-if"),
-    jshint = require("gulp-jshint");
+    jade = require('gulp-jade'),
+    gulpif = require('gulp-if'),
+    jshint = require('gulp-jshint'),
+    run = require('gulp-run');
 
 function clean(path, files) {
   gutil.log(gutil.colors.grey('Clean '+ files +' files.'));
@@ -31,27 +33,27 @@ function clean(path, files) {
 gulp.task('styles', function() {
   clean(config.styles.dest, 'css');
   return gulp.src(config.styles.src)
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(!options.production ? plumber() : gutil.noop())
+    .pipe(!options.production ? sourcemaps.init() : gutil.noop())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({
         browser: [config.browserslist],
         cascade: false }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest (config.styles.dest))
-    .pipe(browserSync.stream());
+    .pipe(!options.production ? gulp.dest (config.styles.dest) : gulp.dest (config.styles.build))
+    .pipe(!options.production ? browserSync.stream() : gutil.noop());
 });
 
 // Compile Jade files into HTML
 gulp.task('templates', function() {
   clean(config.templates.dest, 'html');
   return gulp.src(config.templates.src)
-    .pipe(plumber())
+    .pipe(!options.production ? plumber() : gutil.noop())
     .pipe(jade({
       pretty: true
     }))
-    .pipe(gulp.dest (config.templates.dest))
-    .pipe(browserSync.stream());
+    .pipe(!options.production ? gulp.dest (config.templates.dest) : gulp.dest (config.templates.build))
+    .pipe(!options.production ? browserSync.stream() : gutil.noop());
 });
 
 // JavaScripts things
@@ -63,7 +65,7 @@ gulp.task('scripts', function() {
 });
 
 // Create a server and watch files
-gulp.task('live', ['styles', 'templates'], function() {
+gulp.task('live', function() {
   browserSync.init({
     startPath: config.templates.dest+'/index.html',
     server: {
@@ -74,8 +76,12 @@ gulp.task('live', ['styles', 'templates'], function() {
 
   gulp.watch(config.styles.src, ['styles']);
   gulp.watch(config.templates.src, ['templates']);
+  gulp.watch(config.scripts.src, ['scripts']);
   gulp.watch(config.templates.dest+'/*.html').on('change', browserSync.reload);
 });
 
 // Tests
 gulp.task('test', ['styles', 'templates', 'scripts']);
+
+// Development task
+gulp.task('dev', ['styles', 'templates', 'scripts', 'live']);
